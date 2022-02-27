31c58c41bbc9371d1a139b9188797b3b7cac4e34
Jan Handzel <jhandzel130696@gmail.com>
2022-02-27 20:23:45 +0100

    Created test which check if function correctly add three numbers

--- elp-workshop-1/src/rpn/rpn.test.ts
+++ elp-workshop-1/src/rpn/rpn.test.ts
@@ -12,9 +12,9 @@ describe("rpn()", () => {
       });
 
 
-  it(`correctly adds 2 numbers`, async () => {
-    expect(rpn("2 2 +")).toBe(4);
-  });
+    it(`correctly adds 3 numbers`,  () => { //async
+        expect(rpn("2 2 + 3 *")).toBe(12);
+    });
 
   it("correctly divides  2  numbers", () => {
     expect(rpn("2 2 /")).toBe(1);
